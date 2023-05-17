537271053e1cb10b43ce546e11fb6657f9dce554
JordanSgrigna <kaliwyrdiscord@gmail.com>
2023-05-17 18:06:17 +0200

    Details Page added

--- LaMiaPizzeria/Controllers/MenùController.cs
+++ LaMiaPizzeria/Controllers/MenùController.cs
@@ -1,6 +1,7 @@
 ﻿using LaMiaPizzeria.Database;
 using LaMiaPizzeria.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace LaMiaPizzeria.Controllers
 {
@@ -15,5 +16,23 @@ namespace LaMiaPizzeria.Controllers
             }
             
         }
+
+        public IActionResult Details(int id)
+        {
+            using (PizzaShopContext db = new PizzaShopContext())
+            {
+                Pizza? pizzaDetails = db.Pizza.Where(pizza => pizza.Id == id).FirstOrDefault();
+
+                if(pizzaDetails != null)
+                {
+                    return View(pizzaDetails);
+                }
+                else
+                {
+                    return NotFound($"La pizza con id {id} non è stato trovato");
+                }
+            }
+        }
     }
+
 }
