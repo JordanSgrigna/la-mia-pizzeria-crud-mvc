548e21c39ea6b200e904d34946b86efaa0a5612e
JordanSgrigna <kaliwyrdiscord@gmail.com>
2023-05-17 15:42:43 +0200

    Added Pizza Entity with EFMyPizzaShop DB

---
+++ LaMiaPizzeria/Database/PizzaShopContext.cs
@@ -0,0 +1,16 @@
+﻿using LaMiaPizzeria.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LaMiaPizzeria.Database
+{
+    public class PizzaShopContext : DbContext
+    {
+        public DbSet<Pizza> Pizza { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=EFMyPizzaShop;" +
+                "Integrated Security=True;TrustServerCertificate=True");
+        }
+    }
+}
---
+++ LaMiaPizzeria/Models/Pizza.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LaMiaPizzeria.Models
+{
+    public class Pizza
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Column(TypeName = "text")]
+        public string Description { get; set; }
+
+        [MaxLength(500)]
+        public string ImageUrl { get; set; }
+
+
+        public Pizza(string name, string description, string imageUrl)
+        {
+            Name = name;
+            Description = description;
+            ImageUrl = imageUrl;
+        }
+    }
+}
